c0af79b5c0374daeee1658d5ae0dd4e229303a9c
emiliano1191 <emiliano1191@gmail.com>
2022-11-06 18:36:49 -0300

    se agrego un form para creacion de productos

--- src/services/server.js
+++ src/services/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const { urlencoded } = require('express');
 const mainRouter = require('../routes/index');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
+app.use(express.static('public'));
 app.use('/api', mainRouter);
 
 app.get('/', (req, res) => {
@@ -14,9 +15,7 @@ app.get('/', (req, res) => {
 	})
 })
 
-//este middleware de errores se encarga de atajar todos los errores que haya en nuestras rutas
-//aca metenmos la logica para ver que le respondemos al cliente (si un error generico o uno definido)
-//ojo con las rutas que ejecutan codigo asincronico.
+
 
 app.use((err, req, res, next) => {
 		const status = err.status || 500;
